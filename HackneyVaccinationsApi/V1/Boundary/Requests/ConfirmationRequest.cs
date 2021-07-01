2174a77229cf16704ef47a42816bf648ef8700db
LBHSPreston <selwyn.preston@hackney.gov.uk>
2021-07-01 23:56:54 +0100

    add json property decorators

--- HackneyVaccinationsApi/V1/Boundary/Requests/ConfirmationRequest.cs
+++ HackneyVaccinationsApi/V1/Boundary/Requests/ConfirmationRequest.cs
@@ -1,8 +1,12 @@
+using System.Text.Json.Serialization;
+
 namespace HackneyVaccinationsApi.V1.Boundary.Requests
 {
     public class ConfirmationRequest
     {
+        [JsonPropertyName("email")]
         public string Email { get; set; }
+        [JsonPropertyName("mobileNumber")]
         public string MobileNumber { get; set; }
     }
 }
