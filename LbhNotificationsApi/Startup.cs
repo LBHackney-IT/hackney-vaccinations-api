0b16e8a2bc78ccabb7b360dc1a38c5c0323f1abe
eddygitgub <87383463+EdwardAnoghena@users.noreply.github.com>
2021-11-11 19:14:42 +0100

    Update startup

--- LbhNotificationsApi/Startup.cs
+++ LbhNotificationsApi/Startup.cs
@@ -211,11 +211,6 @@ namespace LbhNotificationsApi
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public static void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-
-            app.UseCors(builder => builder
-                .AllowAnyOrigin()
-                .AllowAnyHeader()
-                .AllowAnyMethod());
             app.UseCorrelation();
             app.UseMiddleware<ExceptionMiddleware>();
 
